e55c714ec38847872100209283c3ff3912c9223e
fwg <adf0001@163.com>
2022-01-11 15:02:02 +0800

    small update: use tpsvr

---
+++ test/test-bundle.bat
@@ -0,0 +1,20 @@
+rem tool from tpsvr @ npm
+
+chcp 65001
+
+set nodeModulesPath=D:\公共项目分类\module\tpsvr/node_modules
+set watchifyPath=%nodeModulesPath%/.bin/watchify
+
+rem title watchify - htm-tool-ui
+
+if not exist ./bundle md bundle
+
+set dest=./bundle/test-bundle.js
+
+"%watchifyPath%" ^
+	-o %dest% ^
+	-v ^
+	-g [ "%nodeModulesPath%/stringify" --extensions [.html .css .htm ] ] ^
+	-r ../package.json:_package_json ^
+	-r ./test-data.js:_test_data ^
+	-r "../htm-tool-ui.js:htm-tool-ui"
